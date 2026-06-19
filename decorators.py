330ed8b16bfd52afa84203f276cd2416d38e1772
AdarshBobade <adarsh.bobade26@gmail.com>
2026-06-19 19:20:24 +0530

    Modified decorators

--- decorators.py
+++ decorators.py
@@ -1,6 +1,6 @@
 import time
 
-def timer(func):
+def decorator_timer_fx(func):
     def wrapper(*args, **kwargs):          # Its a Closure ; *args so it works with ANY function
         start = time.time()
         print('Before!')
@@ -11,7 +11,36 @@ def timer(func):
         return result
     return wrapper
 
-@timer  # Same as saying slow_computation = timer(slow_computation(n))
+class decorator_timer_class(object):
+
+    def __init__(self,original_func):
+        self.original_func = original_func
+
+    def __call__(self,*args,**kwargs):
+        print('call method executed before {}'.format(self.original_func.__name__))
+        return self.original_func(*args,**kwargs)
+    
+def log_call(func):
+    def wrapper(*args, **kwargs):
+        print(f"Calling {func.__name__}")
+        result = func(*args, **kwargs)
+        print(f"Done")
+        return result
+    return wrapper
+
+@log_call
+def ask_ai(question):
+    return "42"
+
+
+
+# @decorator_timer_fx  # Same as saying slow_computation = timer(slow_computation(n))
+# def slow_computation(n):
+#     time.sleep(3)
+#     print('Function is running')
+#     return n * 2
+
+@decorator_timer_class  # Can use a class as a decorator too
 def slow_computation(n):
     time.sleep(3)
     print('Function is running')
